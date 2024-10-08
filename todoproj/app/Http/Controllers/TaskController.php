2ed80f17ac2f4f50b67e4f1602bbc5c84a8b0c01
tonyjose009 <tonyjose009@gmail.com>
2024-10-08 12:50:05 +0100

    Updated services and added new api endpoints

--- todoproj/app/Http/Controllers/TaskController.php
+++ todoproj/app/Http/Controllers/TaskController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\Task\CreateTaskRequest;
+use App\Http\Requests\Task\ListTasksRequest;
+use App\Http\Requests\Task\ReorderTasksRequest;
+use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Services\ProjectService;
+use App\Services\TaskService;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller
 {
     //
+
+    protected ?TaskService $taskService = null;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    public function index()
+    {
+        $projects = (new ProjectService())->getAll();
+//        return  view('tasks.index', compact($projects));
+        return  $projects;
+    }
+
+
+
+    public function list(ListTasksRequest $request)
+    {
+//        echo "reached till here";
+//        exit;
+
+        $tasks = $this->taskService->list($request->get('project_id'));
+        return response()->json([
+            'success' => true,
+            'tasks' => $tasks,
+            'message' => "Tasks retrieved successfully.",
+        ]);
+        // 200
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function reorder()
+    {
+
+    }
 }
--- todoproj/app/Services/TaskService.php
+++ todoproj/app/Services/TaskService.php
@@ -2,10 +2,12 @@
 
 namespace App\Services;
 use App\Models\Task;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\PaginationState;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
+//use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Pagination\Paginator;
@@ -13,10 +15,11 @@ use Illuminate\Pagination\Paginator;
 class TaskService
 {
 
-    public function list(int $projectId){
+    public function list(int $projectId)
+    {
 
-        $tasks = Task::all()->where('project_id', $projectId)->get();
-        return $tasks->orderBy('priority', 'desc');
+        $tasks = Task::all();
+        return  $tasks->sortByDesc('priority');
     }
 
     public function getById(int $projectId){
